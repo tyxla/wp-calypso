64963aff30c82c2399817f22c13b19227ce1bed9
Bernhard Reiter <ockham@raz.or.at>
2016-02-25 16:01:48 +0100

    Themes: Add `package.json` switch
    Add a `package.json` that sets different entry points on the server and in
    the browser (not everything required by `my-sites/themes/index.js` is
    SSR-ready yet, so we need a pruned version for the server).

---
+++ client/my-sites/themes/controller/index.node.js
@@ -0,0 +1,68 @@
+/**
+ * External Dependencies
+ */
+import React from 'react';
+import { Provider as ReduxProvider } from 'react-redux';
+import omit from 'lodash/omit';
+
+/**
+ * Internal Dependencies
+ */
+import { ThemeSheet as ThemeSheetComponent } from 'my-sites/themes/sheet';
+import i18n from 'lib/mixins/i18n';
+import buildTitle from 'lib/screen-title/utils';
+import { getCurrentUser } from 'state/current-user/selectors';
+import { setSection } from 'state/ui/actions';
+import ClientSideEffects from 'components/client-side-effects';
+import LayoutLoggedOut from 'layout/logged-out';
+
+export function makeElement( ThemesComponent, Head, store, props ) {
+	return(
+		<ReduxProvider store={ store }>
+			<Head title={ props.title } tier={ props.tier || 'all' }>
+				<ThemesComponent { ...omit( props, [ 'title', 'runClientAnalytics' ] ) } />
+				<ClientSideEffects>
+					{ props.runClientAnalytics }
+				</ClientSideEffects>
+			</Head>
+		</ReduxProvider>
+	);
+};
+
+// Where do we put this? It's client/server-agnostic, so not in client/controller,
+// which requires ReactDom... Maybe in lib/react-helpers?
+export function makeLoggedOutLayout( context, next ) {
+	const { store, primary, secondary, tertiary } = context;
+	context.layout = (
+		<ReduxProvider store={ store }>
+			<LayoutLoggedOut primary={ primary }
+				secondary={ secondary }
+				tertiary={ tertiary } />
+		</ReduxProvider>
+	);
+	next();
+};
+
+export function details( context, next ) {
+	const user = getCurrentUser( context.store.getState() );
+	const Head = user
+		? require( 'layout/head' )
+		: require( 'my-sites/themes/head' );
+	const props = {
+		themeSlug: context.params.slug,
+		contentSection: context.params.section,
+		title: buildTitle(
+			i18n.translate( 'Theme Details', { textOnly: true } )
+		),
+		isLoggedIn: !! user
+	};
+
+	context.store.dispatch( setSection( 'themes', {
+		hasSidebar: false,
+		isFullScreen: true
+	} ) );
+
+	context.primary = makeElement( ThemeSheetComponent, Head, context.store, props );
+	context.secondary = null; // When we're logged in, we need to remove the sidebar.
+	next();
+}
--- client/my-sites/themes/controller/index.web.js
+++ client/my-sites/themes/controller/index.web.js
@@ -1,26 +1,17 @@
 /**
- * External Dependencies
- */
-import React from 'react';
-import { Provider as ReduxProvider } from 'react-redux';
-import omit from 'lodash/omit';
-
-/**
  * Internal Dependencies
  */
 import SingleSiteComponent from 'my-sites/themes/single-site';
 import MultiSiteComponent from 'my-sites/themes/multi-site';
 import LoggedOutComponent from 'my-sites/themes/logged-out';
-import { ThemeSheet as ThemeSheetComponent } from 'my-sites/themes/sheet';
 import analytics from 'analytics';
 import i18n from 'lib/mixins/i18n';
 import trackScrollPage from 'lib/track-scroll-page';
 import buildTitle from 'lib/screen-title/utils';
-import { getAnalyticsData } from './helpers';
-import { getCurrentUser } from 'state/current-user/selectors';
+import { getAnalyticsData } from '../helpers';
 import { setSection } from 'state/ui/actions';
-import ClientSideEffects from 'components/client-side-effects';
-import LayoutLoggedOut from 'layout/logged-out';
+import { makeElement } from './index.node.js';
+export { details, makeLoggedOutLayout } from './index.node.js';
 
 function getProps( context ) {
 	const { tier, site_id: siteId } = context.params;
@@ -55,33 +46,6 @@ function getProps( context ) {
 		runClientAnalytics
 	};
 }
-
-function makeElement( ThemesComponent, Head, store, props ) {
-	return(
-		<ReduxProvider store={ store }>
-			<Head title={ props.title } tier={ props.tier || 'all' }>
-				<ThemesComponent { ...omit( props, [ 'title', 'runClientAnalytics' ] ) } />
-				<ClientSideEffects>
-					{ props.runClientAnalytics }
-				</ClientSideEffects>
-			</Head>
-		</ReduxProvider>
-	);
-};
-
-// Where do we put this? It's client/server-agnostic, so not in client/controller,
-// which requires ReactDom... Maybe in lib/react-helpers?
-export function makeLoggedOutLayout( context, next ) {
-	const { store, primary, secondary, tertiary } = context;
-	context.layout = (
-		<ReduxProvider store={ store }>
-			<LayoutLoggedOut primary={ primary }
-				secondary={ secondary }
-				tertiary={ tertiary } />
-		</ReduxProvider>
-	);
-	next();
-};
 
 export function singleSite( context, next ) {
 	const Head = require( 'layout/head' );
@@ -123,29 +87,5 @@ export function loggedOut( context, next ) {
 	} ) );
 
 	context.primary = makeElement( LoggedOutComponent, Head, context.store, props );
-	next();
-}
-
-export function details( context, next ) {
-	const user = getCurrentUser( context.store.getState() );
-	const Head = user
-		? require( 'layout/head' )
-		: require( 'my-sites/themes/head' );
-	const props = {
-		themeSlug: context.params.slug,
-		contentSection: context.params.section,
-		title: buildTitle(
-			i18n.translate( 'Theme Details', { textOnly: true } )
-		),
-		isLoggedIn: !! user
-	};
-
-	context.store.dispatch( setSection( 'themes', {
-		hasSidebar: false,
-		isFullScreen: true
-	} ) );
-
-	context.primary = makeElement( ThemeSheetComponent, Head, context.store, props );
-	context.secondary = null; // When we're logged in, we need to remove the sidebar.
 	next();
 }
